a3ecf50c38da63dfb9736e2ca16d8c1898ff0c6e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-07 23:17:17 +0200

    Add files via upload

---
+++ Task03_Oldest_Family_Member/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Family myFamily = new Family();
+
+            for (int i = 1; i <= number; i++)
+            {
+                string[] nextLineData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string curentName = nextLineData[0];
+                int curentAge = int.Parse(nextLineData[1]);
+
+                Person curentPerson = new Person(curentName, curentAge);
+                myFamily.AddMemeber(curentPerson);
+            }
+
+            Person oldestMember = myFamily.GetOldestMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
