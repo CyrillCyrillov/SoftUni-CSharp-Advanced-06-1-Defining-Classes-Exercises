1cc6c58280c5552a0e971dadc54023051e3cc03b
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-07 23:15:38 +0200

    Add files via upload

---
+++ Task01_Define_a_Class_Person/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            personOne.Age = 20;
+            personOne.Name = "Pesho";
+
+            Person personTwo = new Person() { Name = "Gosho", Age = 18 };
+            //personTwo.Name = "Gosho";
+            //personTwo.Age = 18;
+
+            Person personThree = new Person()
+            {
+                Age = 43,
+                Name = "Stamat"
+            };
+            //personThree.Name = "Stamat";
+            //personThree.Age = 43;
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
