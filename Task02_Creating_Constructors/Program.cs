157569850e1f8332628862fb52bece161bc9ec26
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-07 23:16:27 +0200

    Add files via upload

---
+++ Task02_Creating_Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            Person personTwo = new Person(30) ;
+            Person personThree = new Person("Alex", 24);
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
