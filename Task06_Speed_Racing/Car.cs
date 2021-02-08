04b8c2216ce01eee3448883f6b74f83a8cec4226
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-08 22:23:27 +0200

    Add files via upload

---
+++ Task06_Speed_Racing/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task06_Speed_Racing
+{
+    public class Car
+    {
+
+        /*
+        •	string Model
+        •	double FuelAmount
+        •	double FuelConsumptionPerKilometer
+        •	double Travelled distance
+        */
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public bool Drive(double distanceTraveled)
+        {
+            double neededFuel = distanceTraveled * FuelConsumptionPerKilometer;
+
+            if(neededFuel <= FuelAmount)
+            {
+                TravelledDistance += distanceTraveled;
+                FuelAmount -= neededFuel;                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Task06_Speed_Racing/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task06_Speed_Racing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int carNumbers = int.Parse(Console.ReadLine());
+
+            List<Car> carsInRacing = new List<Car>();
+
+            for (int i = 1; i <= carNumbers; i++)
+            {
+                string[] nextData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                //{model} -> nextData[0]
+                //{fuelAmount} -> nextData[1]
+                //{fuelConsumptionFor1km} -> nextData[2]
+
+                Car nextCar = new Car()
+                {
+                    Model = nextData[0],
+                    FuelAmount = double.Parse(nextData[1]),
+                    FuelConsumptionPerKilometer = double.Parse(nextData[2])
+                };
+
+                carsInRacing.Add(nextCar);
+            }
+
+            while (true)
+            {
+                string[] nextComand = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                /*
+                 * Drive -> nextComand[0]
+                 * {carModel} -> nextComand[1]
+                 * {amountOfKm} -> nextComand[2]
+                 * 
+                 */
+                  
+                if(nextComand[0].ToUpper() == "END")
+                {
+                    break;
+                }
+
+                Car curentCar = carsInRacing.FirstOrDefault(n => n.Model == nextComand[1]);
+
+                bool isCanDrive = curentCar.Drive(double.Parse(nextComand[2]));
+
+                if(!isCanDrive)
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+            }
+
+            foreach (Car car in carsInRacing)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
