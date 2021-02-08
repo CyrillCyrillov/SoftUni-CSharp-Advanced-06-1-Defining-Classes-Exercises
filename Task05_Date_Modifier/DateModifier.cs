1bc86dcad20fa253ffbb590eea2adb4f50ddafbc
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-08 22:22:38 +0200

    Add files via upload

---
+++ Task05_Date_Modifier/DateModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public static class DateModifier
+    {
+        public static int CalculateDiff(string stringFirstData, string stringSecondDate)
+        {
+            DateTime firstDate = DateTime.Parse(stringFirstData);
+            DateTime secondDate = DateTime.Parse(stringSecondDate);
+
+            TimeSpan diff = secondDate - firstDate;
+            return Math.Abs(diff.Days);
+
+        }
+
+    }
+}
---
+++ Task05_Date_Modifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int diff = DateModifier.CalculateDiff(firstDate, secondDate);
+
+            Console.WriteLine(diff);
+        }
+    }
+}
