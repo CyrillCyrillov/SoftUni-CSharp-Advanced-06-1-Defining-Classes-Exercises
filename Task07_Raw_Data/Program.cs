0017c2f2a15f8ad0a25a0acbf8bc3c016e50e296
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-08 22:23:58 +0200

    Add files via upload

---
+++ Task07_Raw_Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task07_Raw_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
